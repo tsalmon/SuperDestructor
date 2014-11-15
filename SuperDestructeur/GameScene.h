771566146cde984afde558f7f10d2c65101fbf73
tsalmon <th_s@hotmail.fr>
2014-11-15 18:14:59 +0100

    Initial Commit

---
+++ SuperDestructeur/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  SuperDestructeur
+//
+
+//  Copyright (c) 2014 Salmon. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ SuperDestructeur/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  SuperDestructeur
+//
+//  Created by Salmon on 15/11/2014.
+//  Copyright (c) 2014 Salmon. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
